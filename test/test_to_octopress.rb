c6b1c24208b1753cf9f40a0c2635e2aef0788239
rwwaskk <tsubomi1989@gmail.com>
2015-03-02 12:18:23 -0800

    update test unit

--- test/test_to_octopress.rb
+++ test/test_to_octopress.rb
@@ -4,5 +4,7 @@ require 'minitest/autorun'
 
 class ToOctopressTest < Minitest::Unit::TestCase
     def test_to_octopress   
+        ToOctopress.to_octopress('shawsome_test',"blogs","updated_at","slug","content","title",File.dirname(__FILE__))
+        assert(true,File.file?('to_octopress.zip'))
     end
 end
